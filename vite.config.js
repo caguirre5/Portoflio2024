9efaec6514b2fce2939d98faf40360507bc81d7a
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2024-06-21 17:35:36 -0600

    New Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base:"https://caguirre5.github.io/Portfolio2024/",
+  base:"/Portfolio2024/",
 })
